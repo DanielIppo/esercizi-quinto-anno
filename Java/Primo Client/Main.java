6ddf1961575f5460eceffb07f9919a50cb9c5557
Daniel Ippolito <91868349+DanielIppo@users.noreply.github.com>
2023-11-15 11:06:40 +0000

    Finito il client

--- Java/Primo Client/Main.java
+++ Java/Primo Client/Main.java
@@ -10,17 +10,28 @@ public class Main {
             String serverAddress = myObj.nextLine();
             System.out.print("Inserisci la porta del server: ");
             int serverPort = myObj.nextInt();
-            System.out.print("Inserisci il messaggio da inviare al server: ");
-            String message = myObj.nextLine();
+            System.out.print("Inserisci la versione HTTP: ");
+            String httpVersion = myObj.next();
+            System.out.print("Inserisci la risorsa da chiedere al server: ");
+            String resource = myObj.next();
 
             Socket client = new Socket(serverAddress, serverPort);
 
             DataOutputStream outputServer = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
             BufferedReader inputServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        
+            System.out.println("Richiesta della risorsa al server... \n\n");
+            String request = "GET " + resource + " HTTP/" + httpVersion + "\n\n";
+            outputServer.writeBytes(request);
+            outputServer.flush();
             
-            while((message = inputServer.readLine()) != null){
-                System.out.println(message);
+            // System.out.println(request);
+
+            String line;
+            while((line = inputServer.readLine()) != null){
+                System.out.println(line);
             }
+            client.close();
         } catch (Exception e) {
             System.out.println(e);
         }
