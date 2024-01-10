a0ea569343b880a52a0e19190af274732e24088f
Daniel Ippolito <91868349+DanielIppo@users.noreply.github.com>
2024-01-10 10:05:54 +0000

    Refactor client code to handle user input and server communication

--- Java/server-client/Client.java
+++ Java/server-client/Client.java
@@ -3,29 +3,41 @@ import java.net.*;
 import java.util.Scanner;
 
 public class Client {
+
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         try {
-            System.out.print("Inserisci il metodo che vuoi usare: \n1)Trasforma una stringa in UPPERCASE\n2)Exit");
+            System.out.print(ConsoleColors.BLUE_BOLD + "Inserisci il metodo che vuoi usare: \n"+ ConsoleColors.RED +"1)"+ ConsoleColors.BLUE +"Trasforma una stringa in UPPERCASE\n"+ ConsoleColors.RED +"2)"+ ConsoleColors.BLUE +"Exit\n"+ ConsoleColors.RESET);
             String resource = myObj.next();
-
-            Socket client = new Socket("", 6255);
-
-            DataOutputStream outputServer = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
-            BufferedReader inputServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        
-            System.out.println("Richiesta della risorsa al server... \n\n");
-            String request = "GET " + resource + " HTTP/1.1" + "\n" + "Host: " + "\n\n";
-            outputServer.writeBytes(request);
-            outputServer.flush();
             
-            // System.out.println(request);
+            switch (resource) {
+                case "1":
+                    Socket client = new Socket("", 25565);
 
-            String line;
-            while((line = inputServer.readLine()) != null){
-                System.out.println(line);
+                    DataOutputStream outputServer = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
+                    BufferedReader inputServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                
+                    System.out.println("Richiesta della risorsa al server... \n\n");
+                    String request = "GET " + resource + " HTTP/1.1" + "\n" + "Host: " + "\n\n";
+                    outputServer.writeBytes(request);
+                    outputServer.flush();
+                    
+                    // System.out.println(request);
+        
+                    String line;
+                    while((line = inputServer.readLine()) != null){
+                        System.out.println(line);
+                    }
+                    client.close();
+                    break;
+                case "2":
+                    System.out.println("Arrivederci è stato un piacere!!");
+                    break;
+                default:
+                    break;
             }
-            client.close();
+
+            
         } catch (Exception e) {
             System.out.println(e);
         }
