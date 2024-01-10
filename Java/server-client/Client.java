0f66e5ddeb33cce6ecc8f5a3c1c6fd0378989ed2
Daniel Ippolito <91868349+DanielIppo@users.noreply.github.com>
2024-01-10 11:20:35 +0000

    Refactor client and server code

--- Java/server-client/Client.java
+++ Java/server-client/Client.java
@@ -1,5 +1,11 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Client {
@@ -7,39 +13,44 @@ public class Client {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         try {
-            System.out.print(ConsoleColors.BLUE_BOLD + "Inserisci il metodo che vuoi usare: \n"+ ConsoleColors.RED +"1)"+ ConsoleColors.BLUE +"Trasforma una stringa in UPPERCASE\n"+ ConsoleColors.RED +"2)"+ ConsoleColors.BLUE +"Exit\n"+ ConsoleColors.RESET);
-            String resource = myObj.next();
-            
-            switch (resource) {
-                case "1":
-                    Socket client = new Socket("", 25565);
-
-                    DataOutputStream outputServer = new DataOutputStream(new BufferedOutputStream(client.getOutputStream()));
-                    BufferedReader inputServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            Socket client = new Socket("0.0.0.0", 25565);
+            while (true) {
+            // System.out.print(ConsoleColors.BLUE_BOLD + "Inserisci il metodo che vuoi usare: \n" + ConsoleColors.RED +"1)" + ConsoleColors.BLUE + "Trasforma una stringa in UPPERCASE\n" + ConsoleColors.RED + "2)" + ConsoleColors.BLUE + "Exit\n" + ConsoleColors.RESET);
+                System.out.print("Inserisci il metodo che vuoi usare: \n1)Colora una parola in rosso\n2)Exit\n");
+                String resource = myObj.next();
                 
-                    System.out.println("Richiesta della risorsa al server... \n\n");
-                    String request = "GET " + resource + " HTTP/1.1" + "\n" + "Host: " + "\n\n";
-                    outputServer.writeBytes(request);
-                    outputServer.flush();
-                    
-                    // System.out.println(request);
-        
-                    String line;
-                    while((line = inputServer.readLine()) != null){
-                        System.out.println(line);
-                    }
-                    client.close();
-                    break;
-                case "2":
-                    System.out.println("Arrivederci è stato un piacere!!");
-                    break;
-                default:
-                    break;
-            }
+                switch (resource) {
+                    case "1":
 
+                        System.out.print("Inserisci la parola che vuoi colorare: ");
+                        String frase = myObj.next();
+
+                        BufferedWriter outputServer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+                        BufferedReader inputServer = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                        String request = frase + "\n\n";
+                        outputServer.write(request);
+                        outputServer.flush();
+            
+                        String line = inputServer.readLine();
+                        
+                        System.out.println("Risposta: " + line + "\n\n");
+
+                        
+                        break;
+                    case "2":
+                        client.close();
+                        System.out.println("Arrivederci è stato un piacere!!");
+                        System.exit(0);
+                        break;
+                    default:
+                        System.out.println("Inserisci un numero valido!!");
+                        break;
+                }
+            }
             
         } catch (Exception e) {
             System.out.println(e);
         }
+
     }
 }
--- Java/server-client/Server.java
+++ Java/server-client/Server.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.net.ServerSocket;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class Server {
     private int port;
@@ -36,8 +42,40 @@ public class Server {
     }
 
     public static void main(String[] args) {
-        int port = 8080; // Porta su cui il server ascolta le connessioni
+        int port = 25565; // Porta su cui il server ascolta le connessioni
         Server server = new Server(port);
         server.start();
+    }
+
+    static class ClientHandler implements Runnable {
+        private Socket clientSocket;
+
+        public ClientHandler(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+        }
+
+        //Futuro Daniel implementare il codice del server qua
+        @Override
+        public void run() {
+            System.out.println("New client connected");
+
+            try {
+                BufferedReader inputClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                BufferedWriter outputClient = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+                String line = inputClient.readLine(); 
+                System.out.println("Client request: " + line);
+                outputClient.write("\033[0;31m" + line + "\033[0m\n");
+                outputClient.flush();
+            } catch (Exception e) {
+                System.out.println("Error handling client: " + e.getMessage());
+            } finally {
+                try {
+                    clientSocket.close();
+                } catch (Exception e) {
+                    System.out.println("Error closing the client socket: " + e.getMessage());
+                }
+            }
+        }
     }
 }
